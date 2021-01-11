99cce4912042995f8231f905f76c46a70d651b21
Siydulislam <siydulislam333@gmail.com>
2021-01-11 11:22:57 +0600

    loading

---
+++ src/Tour.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tour = () => {
+    return (
+        <div>
+            tour component
+        </div>
+    );
+};
+
+export default Tour;
---
+++ src/Tours.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tours = () => {
+    return (
+        <div>
+            tours cpmponent
+        </div>
+    );
+};
+
+export default Tours;
