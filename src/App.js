96974a1ffff4ac1a91f15e9464bc596f9d2edf35
Siydulislam <siydulislam333@gmail.com>
2021-01-11 12:24:34 +0600

    refresh btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 
-const url = 'https://course-api.netlify.app/api/react-tours-project';
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newtours = tours.filter((tour) => tour.id !== id);
+    setTours(newtours);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+
+  if(tours.length === 0){
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tours Left</h2>
+          <button className="btn" onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    )
+  }
+
   return (
-    <h2>Tours Project Setup</h2>
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
   );
 }
 
